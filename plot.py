dca81e820011aff963483026918c2912230cc1d8
Bryton Lacquement <lacquement.bryton@protonmail.com>
2018-11-09 11:19:56 +0100

    wip

--- plot.py
+++ plot.py
@@ -8,17 +8,18 @@ parser.add_argument('trace', type=str)
 args = vars(parser.parse_args())
 
 
-x_axis = [pow(2, x) << 10 for x in range(4)]
-for assoc in (pow(2, i) for i in range(4)):
+x_axis = [pow(2, x) << 9 for x in range(5)]
+for assoc, marker in ((1, 'o'), (2, '+'), (4, '*'), (8, '.')):
   results = []
   for x in x_axis:
     misses, reads, writes = simulate(cs=x, assoc=assoc,
                                      **args)
     miss_percentage = misses / (reads + writes) * 100
     results.append(miss_percentage)
-  plt.plot(x_axis, results, label=assoc)
+  plt.plot(x_axis, results, marker + '-', label=assoc)
 
 plt.xticks(x_axis)
+plt.xscale('log', basex=2)
 
 plt.xlabel('Taille du cache (octet)')
 plt.ylabel('Défauts de cache (%)')
